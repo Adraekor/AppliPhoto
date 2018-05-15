af8d42c24cc50462cf54d5e90f3a42fec87e5223
Kaysso <kaysso@live.com>
2018-05-15 23:59:54 +0200

    ajout classe abstraite

---
+++ TagView/TagViewImage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AppliPhoto
+{
+    public class TagViewImage : TagView
+    {
+        public TagViewImage(string iTag, Main iMain) : base(iTag, iMain)
+        {
+        }
+
+        public override void DeleteButtonClick(object sender, EventArgs e)
+        {
+            mMain.DeleteTagImage(mTextBox.Text);
+            Dispose();
+        }
+
+        public override void TagModification(object sender, KeyEventArgs e)
+        {
+            if (mTextBox.Font.Italic)
+                mTextBox.Font = new Font("Arial", 12, FontStyle.Regular);
+            Size size = TextRenderer.MeasureText(mTextBox.Text, mTextBox.Font);
+            mTextBox.Width = size.Width;
+            mButton.Location = new Point(mTextBox.Width, 0);
+        }
+
+        public override void TextValidating(object sender, CancelEventArgs e)
+        {
+            if (mTextBox.Text.Trim() == "")
+                mTextBox.Text = mOldTag;
+
+            mTextBox.Font = new Font("Arial", 12, FontStyle.Italic);
+            Size size = TextRenderer.MeasureText(mTextBox.Text, mTextBox.Font);
+            mTextBox.Width = size.Width;
+            mButton.Location = new Point(mTextBox.Width, 0);
+
+            mMain.ModifyTag(mTextBox.Text, mOldTag);
+        }
+    }
+}
---
+++ TagView/TagViewRecherche.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AppliPhoto
+{
+    public class TagViewRecherche : TagView
+    {
+        //1 = tag recherche; 0 = tag retirer
+        public int type;
+
+        public TagViewRecherche(string iTag, Main iMain,int t) : base(iTag, iMain)
+        {
+            type = t;
+            mTextBox.Enabled = false;
+        }
+
+        public override void DeleteButtonClick(object sender, EventArgs e)
+        {
+            mMain.DeleteTagRecherche(mTextBox.Text,type);
+            Dispose();
+        }
+
+        public override void TagModification(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        public override void TextValidating(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
